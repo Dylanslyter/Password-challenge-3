488458e9415940bd3db31594d70196a1f3d2f1e7
dylan <dylan.slyter@yahoo.com>
2024-02-19 20:46:40 -0500

    Finished challenge 3 with all requirments

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,40 @@
-// Assignment code here
 
+function generatePassword() {
+  let passwordLength = prompt("Enter the desired length for the password:");
+  while(isNaN(passwordLength)  || passwordLength < 8  || passwordLength > 128) {
+    passwordLength = prompt("Password length must be between 8 and 128 characters. Please enter a valid length:");
+  }
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+  let includeUppercase = window.confirm("Include uppercase characters?");
+  let includeLowercase = window.confirm("Include lowercase characters?");
+  let includeNumbers = window.confirm("Include numbers?");
+  let includeSymbols = window.confirm("Include symbols?");
 
-// Write password to the #password input
+  let charset = "";
+  if (includeUppercase) charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  if (includeLowercase) charset += "abcdefghijklmnopqrstuvwxyz";
+  if (includeNumbers) charset += "0123456789";
+  if (includeSymbols) charset += "!@#$%^&*()-_+=<>?";
+
+  if (charset === "") {
+    return "No character set selected. Please include at least one type of character.";
+  }
+  let password = "";
+  for (let i = 0; i < passwordLength; i++) {
+    const randomIndex = Math.floor(Math.random() * charset.length);
+    password += charset[randomIndex];
+  }
+
+  return password;
+}
 function writePassword() {
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
+var generateBtn = document.querySelector("#generate");
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
---
+++ README.md
@@ -0,0 +1,34 @@
+Challenge 3
+
+Creating a random password generator
+
+Description
+
+Using javascript and html to create a random generatory for a password.
+N/A
+
+Usage
+
+Asking you questions to decide on what you want in your passsword and the length of it
+
+Credits
+
+N/A
+
+License
+
+Please refer to the LICENSE in the repo.
+
+🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
+
+Badges
+
+Features
+
+If your project has a lot of features, list them here.
+
+How to Contribute
+
+If you created an application or package and would like other developers to contribute to it, you can include guidelines for how to do so. The Contributor Covenant is an industry standard, but you can always write your own if you'd prefer.
+
+![image](image1.png)
